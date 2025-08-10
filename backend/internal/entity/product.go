45d868555f0050489752d8d2e2c7bf13cae77d62
un-null <kawabata.qppq@gmail.com>
2025-08-10 19:42:23 +0900

    🏷️ add quantity to product entity

--- backend/internal/entity/product.go
+++ backend/internal/entity/product.go
@@ -1,7 +1,8 @@
 package entity
 
 type Product struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
 }
