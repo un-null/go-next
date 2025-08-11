df49b3e6b30b03d02d5d14afcde8f3ea424f92d3
un-null <kawabata.qppq@gmail.com>
2025-08-11 16:20:31 +0900

    🏷️ Modify User struct

--- backend/internal/entity/user.go
+++ backend/internal/entity/user.go
@@ -1,7 +1,13 @@
 package entity
 
+import "time"
+
 type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+	ID        int       `json:"id"` // later > string (uuid)
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Coins     int       `json:"coins"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
